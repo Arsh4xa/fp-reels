0bc55981c6608b8591271462648adb3c05e1d44a
Arsh4xa <ov792000@gmail.com>
2025-03-08 22:43:21 +0700

    Update browserHandler.js

--- backend/browserHandler.js
+++ backend/browserHandler.js
@@ -6,20 +6,27 @@ export async function ReelsUpload(videoPath, caption, cookiesJson) {
     executablePath: '/data/data/com.termux/files/usr/bin/chromium-browser',  // Sesuaikan path Chromium di Termux
     headless: true,  // Ubah ke true jika tidak ingin membuka browser
     args: [
-      '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
       '--no-sandbox', 
       '--disable-setuid-sandbox'
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--disable-software-rasterizer',
     ]
   });
 
   const page = await browser.newPage();
-
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, seperti Gecko) Chrome/115.0.0.0 Safari/537.36'
+  await page.setViewport({ width: 1366, height: 768 });
   // Set cookies
   const cookies = JSON.parse(cookiesJson);
   await page.setCookie(...cookies);
 
   console.log('Mengakses Facebook...');
   await page.goto('https://www.facebook.com/', { waitUntil: 'networkidle2' });
+
+  const htmlContent = await page.evaluate(() => document.body.innerHTML);
+  console.log(htmlContent);
+  page.waitForTimeout(5000); // Tambahkan delay sebelum
 
   await page.screenshot({ path: '/sdcard/check_login.png' }); // Cek apakah berhasil login
 
